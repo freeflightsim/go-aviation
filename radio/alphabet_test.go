5cddadcc5c5a23e5a41aafc5f66d68b86a9af4f8
Peter Morgan <pete@daffodil.uk.com>
2013-03-19 03:54:52 +0000

    testing mode

--- radio/alphabet_test.go
+++ radio/alphabet_test.go
@@ -9,5 +9,13 @@ func TestRadioAlpha( t *testing.T ){
 	
 	fmt.Println("TestRadioAlpha", "---------")
 	
-	fmt.Println("OUT>", GetAjaxAlphabet() )
+	fmt.Println("Spool Alpabet>", GetAjaxAlphabet() )
+	
+	fmt.Println("Test Callsigns", "-------------" )
+	callsigns := []string{"unix09", "win98", "go380", "test123"}
+	for idx, callsign := range callsigns{
+		fmt.Println(">", idx, callsign, Callsign2Words(callsign))
+	}
+	
+	
 }
