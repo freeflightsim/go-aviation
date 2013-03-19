2b3beb97ca72f56618d685c303425a8acbbfb2fa
Peter Morgan <pete@daffodil.uk.com>
2013-03-19 04:16:08 +0000

    push

--- doc.go
+++ doc.go
@@ -4,6 +4,8 @@ go-aviation - Handy stuff for aviation
 The idea is to mash together a basic toolkit and useful for aviation
 all in one package we can all contribute to
 
+Check radio. module
+
 First off it to 
 - Create callsign converters and definition
 - Metar parser
