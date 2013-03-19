c4a0f898092eec4fd0372b8bd0d6474333f8913f
Peter Morgan <pete@daffodil.uk.com>
2013-03-19 04:14:51 +0000

    push

--- doc.go
+++ doc.go
@@ -1,14 +1,5 @@
-package main
-
-// Copyright 2013 Pedro Morgan - pete at freeflightsim dot org 
-//
-// This Source Code Form is subject to the terms of the GPL
-
 /*
-Package main This is a global Bot, to run all virtual flights in the world
-
-go-aviation
-===========
+go-aviation - Handy stuff for aviation
 
 The idea is to mash together a basic toolkit and useful for aviation
 all in one package we can all contribute to
@@ -21,3 +12,5 @@ Looking at this
 https://github.com/mvrak/GoMETAR
 
 */
+package main
+
